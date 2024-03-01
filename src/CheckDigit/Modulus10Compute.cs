48cb10027d99cf0c16119f0c17db8495dea73588
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2024-02-29 21:41:18 -0300

    Modulus 10 calculation

---
+++ src/CheckDigit/Modulus10Compute.cs
@@ -0,0 +1,36 @@
+﻿namespace CheckDigit;
+
+/// <summary>
+/// Modulus 10 calculation.
+/// </summary>
+public class Modulus10Compute : Compute
+{
+    public Modulus10Compute()
+        : base(CalculateMultiplier, CalculateDigit) { }
+
+    private static int CalculateDigit(long sum) => (int)(10 - sum % 10);
+
+    private static int CalculateMultiplier(int multiplier) => multiplier % 2 == 0 ? 1 : 2;
+
+    /// <summary>
+    /// Calculate modulus 10.
+    /// </summary>
+    /// <param name="number">Number to calculate modulus 10</param>
+    /// <returns>Calculated modulus</returns>
+    public override int Calculate(long number)
+    {
+        long sum = 0;
+        int multiplier = 1, multiply;
+        do
+        {
+            multiplier = ComputeMultiplier(multiplier);
+
+            multiply = (int)number % 10 * multiplier;
+
+            sum += (multiply / 10) + (multiply % 10);
+            number /= 10;
+        } while (number > 0);
+
+        return ComputeDigit(sum);
+    }
+}
---
+++ src/CheckDigitTests/Modulus10ComputeTest.cs
@@ -0,0 +1,94 @@
+﻿using CheckDigit;
+
+namespace CheckDigitTests;
+
+public class Modulus10ComputeTest
+{
+    [Fact]
+    public void CalculateLong()
+    {
+        // Arrange
+        long number = 261533;
+        int digit = 4;
+        IModulusCompute compute = new Modulus10Compute();
+
+        // Act
+        int result = compute.Calculate(number);
+
+        // Assert
+        Assert.Equal(digit, result);
+    }
+
+    [Fact]
+    public void CalculateString()
+    {
+        // Arrange
+        string number = "261533";
+        string digit = "4";
+        IModulusCompute compute = new Modulus10Compute();
+
+        // Act
+        string result = compute.Calculate(number);
+
+        // Assert
+        Assert.Equal(digit, result);
+    }
+
+    [Fact]
+    public void ValidateLong()
+    {
+        // Arrange
+        long number = 2615334;
+        IModulusCompute compute = new Modulus10Compute();
+
+        // Act
+        bool result = compute.Validate(number);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void ValidateString()
+    {
+        // Arrange
+        string number = "2615334";
+        IModulusCompute compute = new Modulus10Compute();
+
+        // Act
+        bool result = compute.Validate(number);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void ValidateDigitLong()
+    {
+        // Arrange
+        long number = 261533;
+        int digit = 4;
+        IModulusCompute compute = new Modulus10Compute();
+
+        // Act
+        bool result = compute.Validate(number, digit);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void ValidateDigitString()
+    {
+        // Arrange
+        string number = "261533";
+        string digit = "4";
+        IModulusCompute compute = new Modulus10Compute();
+
+        // Act
+        bool result = compute.Validate(number, digit);
+
+        // Assert
+        Assert.True(result);
+    }
+}
